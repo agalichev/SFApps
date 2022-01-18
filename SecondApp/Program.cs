119c5439d8c0e97fb52a1ae48b40d5f850cc0ebf
Alexander Galichev <lex.galichev@gmail.com>
2022-01-19 01:34:18 +1000

    Delete variables definition. A multipart condition is written.

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -1,5 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string A = "";
-string B = "";
-bool C = A!=B;
+Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+var MyFavoriteColor = Console.ReadLine();
+
+if (MyFavoriteColor == "red")
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is red!");
+}
+
+else if(MyFavoriteColor == "green")
+{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is green!");
+}
+
+else
+{
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is cyan!");
+}
+
+Console.ReadKey();
 
