d2a1ad27ec76c30c8107356fcfa7daa950c73f13
Alexander Galichev <lex.galichev@gmail.com>
2022-01-20 01:57:39 +1000

    Unit 4.2 - Task 4.2.9-4.2.11: for, while, do while

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -1,31 +1,130 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-var MyFavoriteColor = Console.ReadLine();
-
-if (MyFavoriteColor == "red")
+for (int i = 1; i < 4; i++)
 {
-    Console.BackgroundColor = ConsoleColor.Red;
-    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("Iteration {0}", i);
+    switch (Console.ReadLine())
+    {
+        case "red":
+        
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
 
-    Console.WriteLine("Your color is red!");
+        Console.WriteLine("Your color is red!");
+        break;
+
+        case "green":
+        
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is green!");
+        break;
+
+        case "cyan":
+    
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is cyan!");
+        break;
+
+        default:
+        
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is yellow!");
+        break;
+    }
+    
 }
 
-else if(MyFavoriteColor == "green")
-{
-    Console.BackgroundColor = ConsoleColor.Green;
-    Console.ForegroundColor = ConsoleColor.Black;
+int j = 1;
 
-    Console.WriteLine("Your color is green!");
+while(j < 4)
+{
+    Console.WriteLine("Iteration {0}", j);
+    switch(Console.ReadLine())
+    {
+        case "red":
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+            break;
+
+        case "white":
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is white!");
+            break;
+
+        case "blue":
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Your color is blue!");
+            break;
+
+        default:
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+            break;
+    }
+
+    j++;
 }
 
-else
+int t = 0;
+
+do
 {
-    Console.BackgroundColor = ConsoleColor.Cyan;
-    Console.ForegroundColor = ConsoleColor.Black;
+    t++;
+    Console.WriteLine("Iteration {0}", t);
+    switch (Console.ReadLine())
+    {
+        case "red":
 
-    Console.WriteLine("Your color is cyan!");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+            break;
+
+        case "gray":
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is gray!");
+            break;
+
+        case "yellow":
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Your color is yellow!");
+            break;
+
+        default:
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+            break;
+    }
 }
+while (t < 3);
 
-Console.ReadLine();
-
+Console.ReadKey();
