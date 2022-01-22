fe31642d2a244a5e2a564cbe7a7d5239b1bea514
Alexander Galichev <lex.galichev@gmail.com>
2022-01-23 00:12:27 +1000

    Unit 4.3 - Task 4.3.14 "Array of colors. Jagged array".

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -233,7 +233,7 @@ for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
 
 // Task 4.3.12 Array sort, Task 4.3.13 Array elemnsts sum
 
-var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+/*var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
 int temp, sum = 0;
 
@@ -259,4 +259,40 @@ foreach(var item in arr)
 }
 
 Console.WriteLine("Сумма всех элементов массива arr: {0}", sum);
+*/
+
+// Array of colors
+/*string[] favcolors = new string[3];
+
+for(int i = 0; i < favcolors.Length; i++)
+{   
+    Console.WriteLine("Введите свой любимый цвет номер {0}", i + 1);
+    favcolors[i] = Console.ReadLine(); 
+}
+
+foreach(var color in favcolors)
+{
+    foreach (var item in color)
+    {
+        Console.Write(item + " ");
+    }
+}
+*/
+
+// Task 4.3.14 "Jagged array"
+
+int[][] array = new int[3][];
+
+array[0] = new int[2] { 1, 2 };
+array[1] = new int[3] { 1, 2, 3 };
+array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+foreach(var fitems in array)
+{
+    foreach(var sitems in fitems)
+    {
+        Console.Write(sitems + " ");
+    }
+}
+
 Console.ReadKey();
