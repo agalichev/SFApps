5e9c75dbbf769123de8dab58e8940262dc8ef436
Alexander Galichev <lex.galichev@gmail.com>
2022-01-24 00:48:57 +1000

    Added project "Questionnaire.csproj". Unit 4.4 - Task 4.4.2 "Cortege".

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+(string name, int age, string birthdate) quest;
+
+Console.Write("Enter your name: ");
+
+quest.name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+
+quest.age = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Enter your birthdate: ");
+
+quest.birthdate = Console.ReadLine();
+
+Console.WriteLine(" Your name is {0},\n your age is {1},\n and your birthdate is {2} ", quest.name, quest.age, quest.birthdate);
+
+Console.ReadKey();
