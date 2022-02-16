3689de3e1dcb84ab9a9b6c048b167bde117df4ec
Alexander Galichev <lex.galichev@gmail.com>
2022-02-16 23:51:57 +1000

    Unit 7.1 - Task 7.1.4 "Create Employee class inheritors: ProjectManager and Developer".

--- ModuleSevenApp/Program.cs
+++ ModuleSevenApp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace ModuleSevenApp // Note: actual namespace depends on the project name.
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+	#region Task 7.1.4 "Create Employee class inheritors: ProjectManager and Developer"
+	class Employee
+    {
+		public string Name;
+		public int Age;
+		public int Salary;
+    }
+
+	class ProjectManager: Employee
+    {
+		public string ProjectName;
+    }
+
+	class Developer: Employee
+    {
+		private int ProgrammingLanguage;
+    }
+
+    #endregion
+}
