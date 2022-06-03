f9aaadcecd1778d733fec38ec66f7b7401436970
Alexander Galichev <lex.galichev@gmail.com>
2022-06-03 22:11:55 +1000

    Unit 8.1 - Task 8.1.4 : added Drive class.

---
+++ ModuleEightTasks/Drive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleEightTasks
+{
+    internal class Drive
+    {
+        public string Name { get; }
+        public double TotalSpace { get; }
+        public double FreeSpace { get; }
+
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+    }
+}
