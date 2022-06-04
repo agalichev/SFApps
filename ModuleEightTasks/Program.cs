2f9b6beba6016789686af9dac41db9e19c8afbe1
Alexander Galichev <lex.galichev@gmail.com>
2022-06-05 05:49:41 +1000

    Unit 8.2 - Task 8.2.4: Move to trash

--- ModuleEightTasks/Program.cs
+++ ModuleEightTasks/Program.cs
@@ -26,11 +26,13 @@
 
             // Directory using example
             // Получаем все папки и файлы корневого каталога
-            GetCatalogs();
+            //GetCatalogs();
+
+            MoveToTrash();
 
             Console.WriteLine();
 
-            CountFoldersAndFiles(); // Метод подсчета количества папок и файлов в корневом каталоге
+            //CountFoldersAndFiles(); // Метод подсчета количества папок и файлов в корневом каталоге
 
             //CreateNewDirectory(); // Пример создания каталога в папке текущего пользователя
         }
@@ -107,6 +109,21 @@
             }
 
             dirInfo.CreateSubdirectory("NewFolder");
+        }
+
+        static void MoveToTrash() // Task 8.2.4
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\lexga\OneDrive\Рабочий стол\testFolder");
+                string trashPath = @"C:\$Recycle.Bin\testFolder"; 
+
+                dirInfo.MoveTo(trashPath);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
     }
