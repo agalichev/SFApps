0ee234e67b6a8ebb4d4c388e83c42dddc22cba65
Alexander Galichev <lex.galichev@gmail.com>
2022-08-10 17:54:25 +1000

    Added Module10Tasks project

---
+++ Module10Tasks/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Module10Tasks
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+
+        static void Main(string[] args)
+        {
+            // Screencast unit 10.1 practice
+            Logger = new Logger();
+            var worker1 = new Worker1(Logger);
+
+            worker1.Work();
+
+            // Task 10.2.2
+            Writer writer = new Writer();
+            ((IWriter) writer).Write();
+
+            // Task 10.2.3
+            Worker worker = new Worker();
+            worker.Build();
+
+            // Task 10.2.4
+            Worker2 worker2 = new Worker2();
+            ((IWorker1)worker2).Build();
+
+            Console.ReadLine();
+        }
+    }
+
+    // Screencast unit 10.1 practice
+    public interface ILogger
+    {
+        void Error (string message);
+        void Event (string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.WriteLine(message);    
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+
+    // Task 10.1.4
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();   
+    }
+    public class Manager : IManager
+    {
+        public void Create(){}
+
+        public void Read(){}
+
+        public void Update(){}
+
+        public void Delete(){}
+    }
+
+    // Task 10.2.2
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public class Writer : IWriter
+    {
+        void IWriter.Write(){}
+    }
+
+    // Task 10.2.3
+    public interface IWorker1
+    {
+        public void Build();
+    }
+
+    public class Worker : IWorker1
+    {
+        public void Build(){}
+    }
+
+    // Task 10.2.4
+    public class Worker2 : IWorker1
+    {
+        void IWorker1.Build(){}
+    }
+}
---
+++ Module10Tasks/Worker1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10Tasks
+{
+    internal class Worker1 : IWorker
+    {
+        public ILogger Logger { get; }
+
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;    
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker1 начал свою работу!");
+            Thread.Sleep(3000);
+            Logger.Event("Worker1 закончил свою работу!");
+        }
+    }
+}
