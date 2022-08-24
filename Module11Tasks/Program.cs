bef4fd4ed6d41a1f4093ba0cd774344bc742bd84
Alexander Galichev <lex.galichev@gmail.com>
2022-08-24 16:52:27 +1000

    Project Module11Tasks.cs added.

---
+++ Module11Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module11Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
