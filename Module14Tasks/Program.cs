98f9f1e8ea7349c6610873d4263496dd82427350
Alexander Galichev <lex.galichev@gmail.com>
2022-12-18 01:21:09 +1000

    Добавлен проект Module14Tasks.csproj, добавлен класс Tsks14_4.cs.

---
+++ Module14Tasks/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Module14Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 14.4
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var orderedList = Task14_4.GetNameListFromArray(people, 'А');
+
+            orderedList.Sort();
+
+            foreach(var person in orderedList)
+            {
+                Console.WriteLine(person);
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module14Tasks/Task14_4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module14Tasks
+{
+    public static class Task14_4
+    {
+        public static List<string> GetNameListFromArray(string[] names, char letter)
+        {
+            List<string> list = new List<string>();
+
+            foreach(var person in names)
+            {
+                if(person.ToUpper().StartsWith(letter))
+                    list.Add(person);
+            }
+
+            return list;
+        }
+    }
+}
